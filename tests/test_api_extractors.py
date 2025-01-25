0c209d03cb666f3bec5b0ff2dcd6fcf2096ac9e4
devoqub <marattulabaevdev@gmail.com>
2025-01-25 14:54:58 +0400

    Add: api_extractors_tests

---
+++ tests/test_api_extractors.py
@@ -0,0 +1,44 @@
+import json
+import unittest
+
+from src.api_extractors import (
+    BaseAPIExtractor,
+    GeminiAPIExtractor,
+    get_extractor_class
+)
+
+
+class TestBaseAPIExtractor(unittest.TestCase):
+    def test_abc_method(self):
+        class TestBaseClass(BaseAPIExtractor):
+            pass
+
+        with self.assertRaises(TypeError):
+            TestBaseClass()
+
+
+class TestGeminiAPIExtractor(unittest.TestCase):
+    def setUp(self):
+        self.extractor = GeminiAPIExtractor()
+
+    def test_extract_data_success(self):
+        expected_output = "123.45"
+        data_input = json.dumps({
+            "events": [
+                {"price": "123.45"}
+            ]
+        })
+
+        output = self.extractor.extract_data(data_input)
+        self.assertEqual(output, expected_output)
+
+    def test_extract_invalid_data(self):
+        invalid_input = "youfoundme"
+
+        with self.assertRaises(json.JSONDecodeError):
+            self.extractor.extract_data(invalid_input)
+
+    def test_extract_data_missing_price(self):
+        input_data = json.dumps({"events": [{}]})
+        with self.assertRaises(KeyError):
+            self.extractor.extract_data(input_data)
