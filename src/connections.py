597fb0cb0c3f654e9408f44ea8e4e242bc93ab03
devoqub <marattulabaevdev@gmail.com>
2025-01-27 20:14:45 +0400

    Refactored: AioHTTPWSConnection

--- src/connections.py
+++ src/connections.py
@@ -9,7 +9,7 @@ from abc import (
 )
 
 from curl_cffi.requests import AsyncSession
-import curl_cffi.requests.exceptions
+from curl_cffi.requests.exceptions import CurlError
 import aiohttp
 
 import config
@@ -18,18 +18,109 @@ from api_extractors import BaseAPIExtractor
 
 
 class BaseWSConnection(ABC):
-    def __init__(self, url: str, retries: int, retry_timeout: int | float):
+    def __init__(
+            self,
+            url: str,
+            coin_name: str,
+            extractor: Type[BaseAPIExtractor],
+            retries: int = 10,
+            retry_timeout: int | float = 1,
+            retry_connect_timeout: int | float | None = None,
+            *args,
+            **kwargs,
+    ):
         self.url = url
         self.retries = retries
         self.retry_timeout = retry_timeout
+        self.retry_connect_timeout = retry_connect_timeout or config.RETRY_CONNECT_TIMEOUT
+        self.extractor = extractor
+        self.coin_name = coin_name
+        self.greeting_event = kwargs.get("greeting_event", None)
 
-    @abstractmethod
-    async def connect(self):
-        pass
+        self.ticker_value = None
 
     @abstractmethod
     async def listen(self):
+        raise NotImplementedError("This method must be implemented in subclass.")
+
+    @abstractmethod
+    async def _connect(self, session: Type) -> Type:
+        raise NotImplementedError("This method must be implemented in subclass.")
+
+    @abstractmethod
+    async def _handle_message(self, ws: Type) -> Type:
+        raise NotImplementedError("This method must be implemented in subclass.")
+
+    @abstractmethod
+    async def _prepare_message(self, message: str):
+        raise NotImplementedError("This method must be implemented in subclass.")
+
+
+class CurlCffiWSConnection(BaseWSConnection):
+    async def start_listen(self):
         pass
+
+
+class AioHTTPWSConnection(BaseWSConnection):
+    async def listen(self):
+        async with aiohttp.ClientSession() as session:
+            while True:
+                try:
+                    async with await self._connect(session) as ws:
+                        if isinstance(self.greeting_event, asyncio.Event):
+                            self.greeting_event.set()
+
+                        await self._handle_message(ws)
+
+                except asyncio.CancelledError:
+                    print("Task was cancelled")
+                except Exception as e:
+                    print(f"Connection failed: {e}", flush=True)
+                    os.system(f"notify-send 'Polybar Ticker' 'Cannot connect to {self.coin_name}' -t 5000")
+
+                await asyncio.sleep(1)
+
+    async def _connect(self, session: aiohttp.ClientSession) -> aiohttp.ClientWebSocketResponse:
+        attempt = 0
+
+        while attempt <= self.retries:
+            try:
+                ws = await session.ws_connect(self.url, timeout=self.retry_connect_timeout)
+                return ws
+            except aiohttp.ClientError:
+                attempt += 1
+                await asyncio.sleep(self.retry_timeout)
+
+        raise TimeoutError("Не удалось подключится к серверу.")
+
+    async def _prepare_message(self, message: str):
+        try:
+            message = self.extractor.extract_data(data=message)
+
+            self.ticker_value = {'coin_name': self.coin_name, 'message': message}
+
+            # самоуничтожение
+            # os.system(f"notify-send 'Polybar Ticker' '{label}' -u critical -t 2000")
+
+        except json.JSONDecodeError as e:
+            # Ничего не выводим, при подключении часто передается только часть данных, так что это нормально.
+            print("", flush=True)
+        except Exception as e:
+            print(f"Error: {e}")
+
+    async def _handle_message(self, ws: aiohttp.ClientWebSocketResponse) -> None:
+        while not ws.closed:
+
+            msg = await ws.receive()
+            if msg.type == aiohttp.WSMsgType.TEXT:
+                message = msg.data
+                await self._prepare_message(message)
+            elif msg.type in (aiohttp.WSMsgType.ERROR, aiohttp.WSMsgType.CLOSED):
+                break
+
+            await asyncio.sleep(config.UPDATE_TIME)
+            # Сбрасываем накопившиеся сообщения
+            await ws.receive()
 
 
 class WSConnection:
@@ -38,16 +129,12 @@ class WSConnection:
             url: str,
             extractor: Type[BaseAPIExtractor],
             coin_name="COIN",
-            show: bool = False,
-            msg_formatter: Type[mh.MessageFormatter] = mh.DefaultMessageFormatter,
             retry_timeout: int = 1,
             *args,
             **kwargs,
     ):
-        self._formatter: Type[mh.MessageFormatter.handle] = msg_formatter.handle
         self.url = url
         self.coin_name = coin_name
-        self.show = show
         self.retries = 10
         self.retry_timeout = retry_timeout
 
@@ -56,12 +143,6 @@ class WSConnection:
         self.ticker_value = None
         self.greeting_event = kwargs.get("greeting_event", None)
 
-    def set_message_formatter(self, msg_formatter: Type[mh.MessageFormatter]):
-        """Меняем метод отображения тикера"""
-        if not issubclass(msg_formatter, mh.MessageFormatter):
-            raise ValueError("Handler must be an instance of MessageHandler")
-        self._formatter = msg_formatter.handle
-
     async def _connect_ws(self, session: AsyncSession):
         """Подключение по WebSocket с повторными попытками"""
         attempt = 0
@@ -69,7 +150,7 @@ class WSConnection:
             try:
                 ws = await session.ws_connect(self.url, timeout=config.RETRY_CONNECT_TIMEOUT)
                 return ws
-            except curl_cffi.requests.exceptions.CurlError:
+            except CurlError:
                 attempt += 1
                 if attempt >= self.retries:
                     raise
@@ -125,54 +206,6 @@ class WSConnection:
         return f"<WSConnection url={self.url} show={self.show}>"
 
 
-class AioHTTPWSConnection(WSConnection):
-    async def _handle_message(self, ws: aiohttp.ClientWebSocketResponse):
-        while not ws.closed:
-            msg = await ws.receive()
-            if msg.type == aiohttp.WSMsgType.TEXT:
-                data = msg.data
-                message = await self._on_message(data)
-            elif msg.type in (aiohttp.WSMsgType.ERROR, aiohttp.WSMsgType.CLOSED):
-                break
-
-            await asyncio.sleep(config.UPDATE_TIME)
-            # Сбрасываем накопившиеся сообщения
-            await ws.receive()
-
-    async def listen_ws(self):
-        """Прослушиваем соединение по WebSocket"""
-
-        async with aiohttp.ClientSession() as session:
-            while True:
-                try:
-
-                    async with await self._connect_ws(session) as ws:
-                        if isinstance(self.greeting_event, asyncio.Event):
-                            self.greeting_event.set()
-
-                        await self._handle_message(ws)
-
-                except asyncio.CancelledError:
-                    print("Task was cancelled")
-                except Exception as e:
-                    print(f"Connection failed: {e}", flush=True)
-                    os.system(f"notify-send 'Polybar Ticker' 'Cannot connect to {self.coin_name}' -t 5000")
-
-                await asyncio.sleep(1)
-
-    async def _connect_ws(self, session: aiohttp.ClientSession):
-        """Подключение по WebSocket с повторными попытками"""
-        attempt = 0
-        while attempt <= self.retries:
-            try:
-                ws = await session.ws_connect(self.url, timeout=self.retry_timeout)
-                return ws
-            except aiohttp.ClientError:
-                attempt += 1
-                if attempt >= self.retries:
-                    raise
-                await asyncio.sleep(self.retry_timeout)
-
 
 class ConnectionManager:
     def __init__(
@@ -211,7 +244,7 @@ class ConnectionManager:
         task = asyncio.create_task(self.display_value())
         self.active = self.connections[0]
 
-        await asyncio.gather(*[con.listen_ws() for con in self.connections])
+        await asyncio.gather(*[con.listen() for con in self.connections])
 
     async def display_active(self):
         # TODO Rich display (FUTURE)
