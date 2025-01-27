d6cca942308b821ab98ad0b51030d205fab401b5
devoqub <marattulabaevdev@gmail.com>
2025-01-28 01:31:06 +0400

    Refactoring: server class

--- src/actions.py
+++ src/actions.py
@@ -2,24 +2,23 @@ import asyncio
 
 
 class CommandServer:
-    # TODO Refactoring
 
     def __init__(self, manager, host="127.0.0.1", port=14888):
         self.port = port
         self.manager = manager
         self.host = host
+        self.commands = {
+            "next": self.manager.next_connection,
+            "prev": self.manager.prev_connection,
+            "change-handler": self.manager.change_message_formatter,
+        }
 
     async def handle_client(self, reader, writer):
         try:
             data = await reader.read(100)
             command = data.decode().strip()
 
-            if command == "next":
-                await self.manager.next_connection()
-            elif command == "prev":
-                await self.manager.prev_connection()
-            elif command == "change-handler":
-                await self.manager.change_message_formatter()
+            await self.commands[command]
 
             writer.write(b"OK\n")
             await writer.drain()
